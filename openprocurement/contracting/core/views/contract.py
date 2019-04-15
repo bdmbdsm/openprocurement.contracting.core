52dd398920cf6de480bbe87154cd4327ea3c733a
Bohdan Dmytriv <dmytriv@quintagroup.org>
2019-04-15 15:07:00 +0300

    Contract view refactor

--- openprocurement/contracting/core/views/contract.py
+++ openprocurement/contracting/core/views/contract.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 from openprocurement.api.utils import (
     APIResourceListing,
-    context_unpack,
     json_view,
-    set_ownership,
 )
 from openprocurement.contracting.core.utils import (
     contractingresource,
     contract_serialize,
-    save_contract
 )
 from openprocurement.contracting.core.design import (
     FIELDS,
@@ -19,7 +16,9 @@ from openprocurement.contracting.core.design import (
     contracts_real_by_local_seq_view,
     contracts_test_by_local_seq_view,
 )
-from openprocurement.contracting.core.validation import validate_contract_data
+from openprocurement.api.utils.validation import validate_data_to_event
+from openprocurement.api.utils.error_management import handle_errors_on_view
+from openprocurement.contracting.core.manager_discovery import ContractManagerDiscovery
 
 VIEW_MAP = {
     u'': contracts_real_by_dateModified_view,
@@ -57,23 +56,11 @@ class ContractsResource(APIResourceListing):
         self.log_message_id = 'contract_list_custom'
 
     @json_view(content_type="application/json", permission='create_contract',
-               validators=(validate_contract_data,))
+               validators=(validate_data_to_event,))
+    @handle_errors_on_view
     def post(self):
-        contract = self.request.validated['contract']
-        for i in self.request.validated['json_data'].get('documents', []):
-            doc = type(contract).documents.model_class(i)
-            doc.__parent__ = contract
-            contract.documents.append(doc)
-
-        acc = set_ownership(contract, self.request)
-        self.request.validated['contract'] = contract
-        self.request.validated['contract_src'] = {}
-        if save_contract(self.request):
-            self.LOGGER.info('Created contract {} ({})'.format(contract.id, contract.contractID),
-                             extra=context_unpack(self.request, {'MESSAGE_ID': 'contract_create'},
-                                                  {'contract_id': contract.id, 'contractID': contract.contractID or ''}))
-            self.request.response.status = 201
-            return {
-                'data': contract.serialize("view"),
-                'access': acc,
-            }
+        event = self.request.event
+        md = ContractManagerDiscovery(self.request.registry.manager_registry)
+        manager = md.discover(event.data)()
+        self.request.response.status = 201
+        return manager.create_contract(event)
