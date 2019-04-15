6a584c165fd0f0f7b0b6d253ecbf307fd3507832
Bohdan Dmytriv <dmytriv@quintagroup.org>
2019-04-15 14:54:04 +0300

    ManagerDiscovery for contracting

---
+++ openprocurement/contracting/core/manager_discovery.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from openprocurement.api.utils.manager_discovery import ManagerDiscovery
+from openprocurement.api.exceptions import CorniceErrors
+from openprocurement.contracting.core.constants import DEFAULT_CONTRACT_TYPE
+
+
+class ContractManagerDiscovery(ManagerDiscovery):
+
+    _contract_type_field_name = 'contractType'
+
+    def __init__(self, manager_registry):
+        self._manager_registry = manager_registry
+
+    def discover(self, data):
+        contract_type_name = data.get(self._contract_type_field_name, DEFAULT_CONTRACT_TYPE)
+
+        manager = self._manager_registry.get_manager(contract_type_name)
+
+        if not manager:
+            raise CorniceErrors(
+                415,
+                ('body', 'data', 'contractType Not implemented')
+            )
+
+        return manager
