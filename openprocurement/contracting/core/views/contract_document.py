966d9b3eda79d4217b43b001228f93e978ea0d6a
Bohdan Dmytriv <dmytriv@quintagroup.org>
2019-04-15 15:24:51 +0300

    Contract Document view refactor

--- openprocurement/contracting/core/views/contract_document.py
+++ openprocurement/contracting/core/views/contract_document.py
@@ -2,28 +2,18 @@
 from openprocurement.api.utils import (
     APIResource,
     json_view,
-    context_unpack,
     get_file,
 )
-from openprocurement.api.validation import (
-    validate_file_update,
-    validate_file_upload,
-)
 from openprocurement.contracting.core.utils import (
-    apply_patch,
     contractingresource,
-    save_contract,
-)
-from openprocurement.contracting.core.interfaces import (
-    IContractManager,
 )
 from openprocurement.contracting.core.constants import (
     ENDPOINTS,
 )
-from openprocurement.contracting.core.validation import (
-    validate_contract_document,
-    validate_patch_contract_document,
-)
+from openprocurement.api.utils.validation import validate_data_to_event
+from openprocurement.contracting.core.manager_discovery import ContractManagerDiscovery
+from openprocurement.api.utils.documents import DocumentUploadReader
+from openprocurement.api.utils.error_management import handle_errors_on_view
 
 
 @contractingresource(
@@ -34,32 +24,17 @@ class CeasefireContractDocumentResource(APIResource):
 
     @json_view(
         content_type="application/json",
-        validators=(validate_file_upload,),
+        validators=(validate_data_to_event,),
         permission='edit_contract')
+    @handle_errors_on_view
     def collection_post(self):
-        contract = self.request.validated['contract']
-        document = self.request.validated['document']
-        manager = self.request.registry.getAdapter(contract, IContractManager).document_manager()
-        manager.create_document(self.request)
-        if save_contract(self.request):
-            self.LOGGER.info(
-                'Created contract document. contract ID: {0} documentID: {1}'.format(
-                    contract.id,
-                    document.id
-                ),
-                extra=context_unpack(
-                    self.request,
-                    {'MESSAGE_ID': 'contract_document_create'},
-                    {
-                        'contract_id': contract.id,
-                        'document_id': document.id
-                    }
-                )
-            )
-            self.request.response.status = 201
-            return {
-                'data': document.serialize("view"),
-            }
+        event = self.request.event
+        md = ContractManagerDiscovery(self.request.registry.manager_registry)
+        manager = md.discover(event.ctx.high).document_manager()
+        upl_rdr = DocumentUploadReader()
+        event.ctx.cache.document = upl_rdr.read(self.request)
+        self.request.response.status = 201
+        return manager.create_document(event)
 
     @json_view(content_type="application/json", permission='view_listing')
     def get(self):
@@ -70,53 +45,24 @@ class CeasefireContractDocumentResource(APIResource):
 
     @json_view(
             content_type="application/json",
-            validators=(validate_patch_contract_document,),
+            validators=(validate_data_to_event,),
             permission='edit_contract')
+    @handle_errors_on_view
     def patch(self):
-        contract = self.request.validated['contract']
-        document = self.request.context
-        manager = self.request.registry.getAdapter(contract, IContractManager).document_manager()
-        manager.change_document(self.request)
-
-        if apply_patch(self.request):
-            self.LOGGER.info(
-                'Updated ceasefire contract document. '
-                'contractID: {0} documentID: {1}'.format(
-                    contract.id,
-                    document.id,
-                ),
-                extra=context_unpack(
-                    self.request,
-                    {'MESSAGE_ID': 'ceasefire_contract_document_patch'}
-                    )
-                )
-            return {'data': document.serialize('view')}
+        event = self.request.event
+        md = ContractManagerDiscovery(self.request.registry.manager_registry)
+        manager = md.discover(event.ctx.high).document_manager()
+        return manager.change_document(event)
 
     @json_view(
         content_type="application/json",
-        validators=(validate_file_update,),
+        validators=(validate_data_to_event,),
         permission='edit_contract')
+    @handle_errors_on_view
     def put(self):
-        contract = self.request.context.__parent__
-        document = self.request.validated['document']
-        manager = self.request.registry.getAdapter(contract, IContractManager).document_manager()
-        manager.put_document(self.request)
-        if save_contract(self.request):
-            self.LOGGER.info(
-                'Created contract document. contract ID: {0} documentID: {1}'.format(
-                    contract.id,
-                    document.id
-                ),
-                extra=context_unpack(
-                    self.request,
-                    {'MESSAGE_ID': 'contract_document_create'},
-                    {
-                        'contract_id': contract.id,
-                        'document_id': document.id
-                    }
-                )
-            )
-            self.request.response.status = 200
-            return {
-                'data': document.serialize("view"),
-            }
+        event = self.request.event
+        md = ContractManagerDiscovery(self.request.registry.manager_registry)
+        manager = md.discover(event.ctx.high).document_manager()
+        upl_rdr = DocumentUploadReader()
+        event.ctx.cache.document = upl_rdr.read(self.request)
+        return manager.put_document(event)
